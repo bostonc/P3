f2f266866fc98403dcd5ac191913980292b227ac
mrmintz <mrmintz@umich.edu>
2017-03-20 21:19:52 -0400

    commented out destructor

--- bnode_leaf.cpp
+++ bnode_leaf.cpp
@@ -1,15 +1,15 @@
 #include "bnode_leaf.h"
-//#include <vector>
+#include <vector>
 
 using namespace std;
 
 Bnode_leaf::~Bnode_leaf() {
-    // Remember to deallocate memory!!
-	for (int i = 0; i < num_values; ++i)
-	{
-		delete values[i];
-	}	
-	//delete[] values;
+//     // Remember to deallocate memory!!
+// 	for (int i = 0; i < num_values; ++i)
+// 	{
+// 		delete values[i];
+// 	}	
+// 	//delete[] values;
 }
 
 VALUETYPE Bnode_leaf::merge(Bnode_leaf* rhs) {
