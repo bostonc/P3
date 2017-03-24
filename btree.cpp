ba593763f1286c57e5a204a4ed5c079788b6360a
mrmintz <mrmintz@umich.edu>
2017-03-23 23:51:21 -0400

    remove - leaf parents

--- btree.cpp
+++ btree.cpp
@@ -183,27 +183,48 @@ bool Btree::remove(VALUETYPE value) {
 			if (leaf->next && leaf->next->getNumValues() > BTREE_LEAF_SIZE / 2) {
 				//redistribute with right node (next)
 				VALUETYPE new_parent_val = leaf->redistribute(leaf->next);
-				leaf->parent->insert(new_parent_val);
+				if (leaf->parent == leaf->next->parent) {
+					leaf->parent->insert(new_parent_val);
+				}
+				else {
+					//find common ancestor
+				}
 				
 			}
 			else if (leaf->prev && leaf->getNumValues() > BTREE_LEAF_SIZE / 2) {
 				//redistribute with left node (prev)
 				VALUETYPE new_parent_val = leaf->prev->redistribute(leaf);
-				leaf->parent->insert(new_parent_val);
+				if (leaf->parent == leaf->prev->parent) {
+					leaf->parent->insert(new_parent_val);
+				}
+				else {
+					//find common ancestor
+				}
 				
 			}
 			//if not, merge with a node
 			else if (leaf->next) {
 				//merge with right node (next)
 				VALUETYPE new_parent_val = leaf->merge(leaf->next);
-				leaf->parent->insert(new_parent_val);
+				if (leaf->parent == leaf->next->parent) {
+					
+					leaf->parent->insert(new_parent_val);
+				}
+				else {
+					//find common ancestor
+				}
 				//fix tree
 				
 			}
 			else if (leaf->prev) {
 				//merge with left node (prev)
 				VALUETYPE new_parent_val = leaf->prev->merge(leaf);
-				leaf->parent->insert(new_parent_val);
+				if (leaf->parent == leaf->prev->parent) {
+					leaf->parent->insert(new_parent_val);
+				}
+				else {
+					//find common ancestor
+				}
 				//fix tree
 			}
 			else {
