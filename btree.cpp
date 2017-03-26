84182578f954fb81971c6f3ecc39bd39946763ce
mrmintz <mrmintz@umich.edu>
2017-03-26 17:54:09 -0400

    working on remove

--- btree.cpp
+++ btree.cpp
@@ -324,7 +324,7 @@ bool Btree::remove(VALUETYPE value) {
 	if (done) {
 		//check asserts and return true
 		assert(isValid());
-		assert(leaf->getNumValues() >= BTREE_LEAF_SIZE / 2 && leaf->getNumValues() <= BTREE_LEAF_SIZE());
+		assert(leaf->getNumValues() >= BTREE_LEAF_SIZE / 2 && leaf->getNumValues() <= BTREE_LEAF_SIZE);
 		return true;
 	}
 	//shouldn't get here
