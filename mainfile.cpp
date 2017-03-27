1d92c6dc3313aff769d16e713c22621da51d66d6
mrmintz <mrmintz@umich.edu>
2017-03-27 16:34:58 -0400

    testing

--- mainfile.cpp
+++ mainfile.cpp
@@ -260,9 +260,9 @@ int main() {
     //freopen(filename.c_str(),"w",stdout); //Comment out if you want to write to a file. You should to set the
                                             //values in constants.h to create the corresponding output
     
-    small_test();
-    splitTestFromSpec();
-    //testForRedistribution();
+    small_test(); //passing!
+    splitTestFromSpec(); //passing!
+    testForRedistribution();
     //large_test1();
     //large_test2();
     //stress_insert(500);
